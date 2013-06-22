ea580ce777eae3b26fcc034271828707193cc585
stashuk <sashko.stashuk@gmail.com>
2013-06-22 16:39:39 +0200

    Benchmarking scripts for workerskill.

---
+++ benchmarks/workerskill/benchmark_single.sh
@@ -0,0 +1,53 @@
+# $1: instance
+# $2: timeout
+
+# default parameters
+export PATH=$1
+export LD_LIBRARY_PATH=$2
+instance=$3
+to=$4
+
+confstr="--cspenable"
+
+# split configurations
+IFS=';' read -ra confs <<< "$confstr"
+header="#size"
+i=0
+for c in "${confs[@]}"
+do
+	header="$header   \"$c\""
+	let i=i+1
+done
+echo $header
+
+# do benchmark
+echo -ne "$instance"
+
+# write HEX program
+prog="
+$dom(5..10)
+"
+for (( j = 1; j <= instance; j++ ))
+do
+	prog="domain($j). $prog"
+done
+echo $prog > prog$instance.hex
+
+# for all configurations
+i=0
+for c in "${confs[@]}"
+do
+	echo -ne -e " "
+	$(timeout $to time -o $instance.time.dat -f %e dlvhex2 $c --plugindir=../../../local/lib simple.hex 2>/dev/null >/dev/null)
+	ret=$?
+	output=$(cat $instance.time.dat)
+	if [[ $ret == 124 ]]; then
+		output="---"
+	fi
+	echo -ne $output
+	# rm $instance.time.dat
+	let i=i+1
+done
+echo -e -ne "\n"
+
+rm prog$instance.hex
